683ad4b8e1120a22ab95b17eb49977d101660f73
Pablo <instypablo@gmail.com>
2022-10-03 19:43:11 +0200

    Mitad del ejercicio

--- Tema1/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Tema1/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView cColor = (TextView) findViewById(R.id.tvChangeColor);
+        TextView tvRed = (TextView) findViewById(R.id.tvRed);
+        TextView tvGreen = (TextView) findViewById(R.id.tvGreen);
+        TextView tvBlue = (TextView) findViewById(R.id.tvBlue);
+
+        SeekBar sbRed = (SeekBar) findViewById(R.id.sbRed);
+        SeekBar sbGreen = (SeekBar) findViewById(R.id.sbGreen);
+        SeekBar sbBlue = (SeekBar) findViewById(R.id.sbBlue);
+        
     }
 }
