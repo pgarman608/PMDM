771fd6f94cd9fccb6c3bdd9ef242f7bde427ab96
Pablo <instypablo@gmail.com>
2022-10-17 16:34:45 +0200

    Actividad terminada

--- Tema1/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Tema1/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btToast = (Button) findViewById(R.id.buttonT);
+        Button btAlert = (Button) findViewById(R.id.buttonA);
+        Button btSnack = (Button) findViewById(R.id.buttonS);
+
+        btToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toastLargo("Esto es un toast");
+            }
+        });
+        btAlert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog alertd = createAlertDialog("Alert","Esto es un alert");
+                alertd.show();
+            }
+        });
+        btSnack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view,"Esto es un SnakBar",Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private AlertDialog createAlertDialog(String titulo, String txt) {
+        AlertDialog.Builder ad = new AlertDialog.Builder(MainActivity.this);
+        ad.setMessage(txt).setTitle(titulo);
+        ad.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this,"SIIII",Toast.LENGTH_SHORT).show();
+            }
+        });
+        ad.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this,"Noooo",Toast.LENGTH_SHORT).show();
+            }
+        });
+        return ad.create();
+    }
+
+    private void toastLargo(String txt) {
+        Toast.makeText(MainActivity.this,txt,Toast.LENGTH_LONG).show();
     }
 }
