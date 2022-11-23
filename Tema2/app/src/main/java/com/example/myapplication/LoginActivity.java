e91a912d6a1106d50917dbc2531a74bc55a2059c
Pablo <instypablo@gmail.com>
2022-11-23 21:16:42 +0100

    3%

--- Tema2/app/src/main/java/com/example/myapplication/LoginActivity.java
+++ Tema2/app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
@@ -9,5 +11,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getWindow().setStatusBarColor(Color.parseColor("#FCC2FC"));
+        getSupportActionBar().hide();
     }
 }
